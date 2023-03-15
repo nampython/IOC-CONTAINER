aceb40b6f82ab613beafd5d4ac72f067290eacb3
NamQuang13 <dragonsangsang10a1@gmail.com>
2023-03-15 20:50:39 +0700

    Message = change order of some method and add docs for method

--- src/main/java/org/ioc/EnqueuedComponentDetails.java
+++ src/main/java/org/ioc/EnqueuedComponentDetails.java
@@ -12,10 +12,8 @@ import java.util.LinkedList;
 public class EnqueuedComponentDetails {
     private final ComponentModel componentModel;
     private final LinkedList<DependencyParam> constructorParams;
-
-    private Object[] constructorInstances;
     private final LinkedList<DependencyParam> fieldDependencies;
-
+    private Object[] constructorInstances;
     private Object[] fieldInstances;
 
     public EnqueuedComponentDetails(ComponentModel componentModel) {
@@ -26,47 +24,39 @@ public class EnqueuedComponentDetails {
         this.fillFieldDependencyTypes();
     }
 
-    private void fillFieldDependencyTypes() {
-        for (Field autowireAnnotatedField : this.componentModel.getAutowireAnnotatedFields()) {
-            Class<?> type = autowireAnnotatedField.getType();
-            String instanceName = this.getInstanceName(autowireAnnotatedField.getDeclaredAnnotations());
-            Annotation[] annotations = autowireAnnotatedField.getDeclaredAnnotations();
-            Type genericType = autowireAnnotatedField.getGenericType();
-            this.fieldDependencies.add(this.createDependencyParam(
-                    type,
-                    instanceName,
-                    annotations,
-                    genericType
-            ));
-        }
-    }
-
+    /**
+     * This function iterates through the constructor parameters of the component and creates a dependency parameter for
+     * each one
+     */
     private void fillConstructorParams() {
         for (Parameter parameter : this.componentModel.getTargetConstructor().getParameters()) {
             Class<?> type = parameter.getType();
             String instanceName = this.getInstanceName(parameter.getDeclaredAnnotations());
             Annotation[] annotations = parameter.getDeclaredAnnotations();
             Type parameterizedType = parameter.getParameterizedType();
-            this.constructorParams.add(this.createDependencyParam(
-                    type,
-                    instanceName,
-                    annotations,
-                    parameterizedType
-            ));
+            this.constructorParams.add(this.createDependencyParam(type, instanceName, annotations, parameterizedType));
         }
     }
 
-
-    private DependencyParam createDependencyParam(Class<?> type,
-                                                  String instanceName,
-                                                  Annotation[] annotations,
-                                                  Type parameterizedType) {
-        if (Collection.class.isAssignableFrom(type)) {
-            return new DependencyParamCollection((ParameterizedType) parameterizedType, type, instanceName, annotations);
+    /**
+     * This function is used to fill the fieldDependencies list with the dependencies of the fields of the component
+     */
+    private void fillFieldDependencyTypes() {
+        for (Field autowireAnnotatedField : this.componentModel.getAutowireAnnotatedFields()) {
+            Class<?> type = autowireAnnotatedField.getType();
+            String instanceName = this.getInstanceName(autowireAnnotatedField.getDeclaredAnnotations());
+            Annotation[] annotations = autowireAnnotatedField.getDeclaredAnnotations();
+            Type genericType = autowireAnnotatedField.getGenericType();
+            this.fieldDependencies.add(this.createDependencyParam(type, instanceName, annotations, genericType));
         }
-        return new DependencyParam(type, instanceName, annotations);
     }
 
+    /**
+     * It gets the value of the @Qualifier annotation
+     *
+     * @param annotations - The annotations of the field to be injected.
+     * @return - The name of the instance.
+     */
     private String getInstanceName(Annotation[] annotations) {
         final Annotation annotation = HandlerAnnotation.getAnnotation(annotations, Qualifier.class);
         if (annotation != null) {
@@ -81,43 +71,69 @@ public class EnqueuedComponentDetails {
         return null;
     }
 
+    /**
+     * If the type is a collection, create a DependencyParamCollection, otherwise create a DependencyParam
+     *
+     * @param type              - The type of the parameter.
+     * @param instanceName      - The name of the instance to be injected.
+     * @param annotations       - The annotations on the parameter.
+     * @param parameterizedType - The type of the parameter.
+     * @return - A DependencyParam object.
+     */
+    private DependencyParam createDependencyParam(Class<?> type, String instanceName, Annotation[] annotations, Type parameterizedType) {
+        return Collection.class.isAssignableFrom(type) ? new DependencyParamCollection((ParameterizedType) parameterizedType, type, instanceName, annotations) : new DependencyParam(type, instanceName, annotations);
+    }
+
+    /**
+     * > If the constructorInstances array is null, then create a new array of objects by mapping the constructorParams
+     * stream to the instance of each dependency param
+     *
+     * @return - An array of objects that are the instances of the constructor parameters.
+     */
     public Object[] getConstructorInstances() {
         if (this.constructorInstances == null) {
-            this.constructorInstances = this.constructorParams.stream()
-                    .map(DependencyParam::getInstance)
-                    .toArray(Object[]::new);
+            this.constructorInstances = this.constructorParams.stream().map(DependencyParam::getInstance).toArray(Object[]::new);
         }
         return constructorInstances;
     }
+
+    /**
+     * > If the fieldInstances array is null, then create a new array of objects by mapping the fieldDependencies stream to
+     * the instance of each dependency parameter, and return the array
+     *
+     * @return - An array of objects that are the instances of the field dependencies.
+     */
     public Object[] getFieldInstances() {
         if (this.fieldInstances == null) {
-            this.fieldInstances = this.fieldDependencies.stream()
-                    .map(DependencyParam::getInstance)
-                    .toArray(Object[]::new);
+            this.fieldInstances = this.fieldDependencies.stream().map(DependencyParam::getInstance).toArray(Object[]::new);
         }
         return this.fieldInstances;
     }
+
+    /**
+     * > This function returns the constructor parameters of the class
+     *
+     * @return - A LinkedList of DependencyParam objects.
+     */
     public LinkedList<DependencyParam> getConstructorParams() {
         return constructorParams;
     }
 
+    /**
+     * > This function returns a list of dependencies for the field
+     *
+     * @return - A list of dependencies.
+     */
     public LinkedList<DependencyParam> getFieldDependencies() {
         return fieldDependencies;
     }
 
+    /**
+     * This function returns the component model.
+     *
+     * @return - The componentModel
+     */
     public ComponentModel getComponentModel() {
         return componentModel;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuffer sb = new StringBuffer("EnqueuedComponentDetails{");
-        sb.append("componentModel=").append(componentModel);
-        sb.append(", constructorParams=").append(constructorParams);
-        sb.append(", constructorInstances=").append(constructorInstances == null ? "null" : Arrays.asList(constructorInstances).toString());
-        sb.append(", fieldDependencies=").append(fieldDependencies);
-        sb.append(", fieldInstances=").append(fieldInstances == null ? "null" : Arrays.asList(fieldInstances).toString());
-        sb.append('}');
-        return sb.toString();
     }
 }
