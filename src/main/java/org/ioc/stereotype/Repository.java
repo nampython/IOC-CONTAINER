ba13357de82b16fdf80a36265a49c4cc829e340d
NamQuang13 <dragonsangsang10a1@gmail.com>
2023-03-19 20:56:35 +0700

    Message = Added @Configuration and @Repository

---
+++ src/main/java/org/ioc/stereotype/Repository.java
@@ -0,0 +1,13 @@
+package org.ioc.stereotype;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@AliasFor(Component.class)
+public @interface Repository {
+}
+
