0ab1f4fc2609bdd011aa0a46bb3b366acbf4a0a5
NamQuang13 <dragonsang10a1@gmail.com>
2023-03-21 09:27:31 +0700

    Message = implement getDefineBean and getImplementations in ApplicationContextCached.java

--- src/main/java/org/ioc/contex/ApplicationContextCached.java
+++ src/main/java/org/ioc/contex/ApplicationContextCached.java
@@ -5,7 +5,7 @@ import org.ioc.contex.factory.ApplicationContextInternal;
 
 import java.lang.annotation.Annotation;
 import java.util.*;
-
+//TODO: implement getComponentByAnnotation
 public class ApplicationContextCached extends ApplicationContextInternal {
     private final Map<Class<?>, ComponentModel> cachedComponents;
     private final Map<Class<?>, Collection<ComponentModel>> cachedImplementations;
@@ -16,5 +16,29 @@ public class ApplicationContextCached extends ApplicationContextInternal {
         this.cachedImplementations = new HashMap<>();
         this.cachedComponentsByAnnotation = new HashMap<>();
         super.init(localClasses, components);
+    }
+
+    @Override
+    public ComponentModel getDefineBean(Class<?> componentType) {
+        if (this.cachedComponents.containsKey(componentType)) {
+            return this.cachedComponents.get(componentType);
+        } else {
+            final ComponentModel componentModel = super.getDefineBean(componentType);
+            if (componentModel != null) {
+                this.cachedComponents.put(componentType, componentModel);
+            }
+            return componentModel;
+        }
+    }
+
+    @Override
+    public Collection<ComponentModel> getImplementations(Class<?> cls) {
+        if (this.cachedImplementations.containsKey(cls)) {
+            return this.cachedImplementations.get(cls);
+        } else {
+            final Collection<ComponentModel> implementations = super.getImplementations(cls);
+            this.cachedImplementations.put(cls, implementations);
+            return implementations;
+        }
     }
 }
